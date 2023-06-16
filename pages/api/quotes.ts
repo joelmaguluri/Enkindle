bb938cff4dc9f7ee9b2fa66047221ced0245b86e
Bhupen / Sudeep @gridflowAI <gridflowai@gmail.com>
2023-06-16 09:31:26 -0700

    WIP

--- pages/api/quotes.ts
+++ pages/api/quotes.ts
@@ -1,17 +1,9 @@
-import router from "@/app/common/db";
-import { MongoClient, Db } from "mongodb";
-import { NextApiRequest, NextApiResponse } from "next";
-
-interface CustomRequest extends NextApiRequest {
-  dbClient?: MongoClient;
-  db?: Db;
-}
+import db from "@/app/common/db";
 
 const QUOTES_PER_PAGE = 25;
 
 // Route handler
-router.get(async (req: CustomRequest, res: NextApiResponse) => {
-  const db = req.db;
+export default async function handler(req: any, res: any) {
   const page = parseInt(req.query.page as string) || 1;
 
   try {
@@ -40,11 +32,4 @@ router.get(async (req: CustomRequest, res: NextApiResponse) => {
     console.error("Error fetching quotes:", error);
     res.status(500).json({ error: "Error fetching quotes" });
   }
-});
-
-function onError(err: unknown, req: CustomRequest, res: NextApiResponse) {
-  console.log(err);
-  res.status(500).end("Internal server error");
 }
-
-export default router.handler({ onError });
--- src/app/common/db/index.ts
+++ src/app/common/db/index.ts
@@ -1,25 +1,7 @@
-import { MongoClient, Db } from "mongodb";
-import { NextApiRequest, NextApiResponse } from "next";
-import { createRouter } from "next-connect";
-
-interface CustomRequest extends NextApiRequest {
-  dbClient?: MongoClient;
-  db?: Db;
-}
+import { MongoClient } from "mongodb";
 
 const client = new MongoClient(process.env.DATABASE_URI || "");
 
-const router = createRouter<CustomRequest, NextApiResponse>();
+const db = client.db("Enkindle");
 
-router.use(async (req, res, next) => {
-  console.log("connected");
-  if (client.db.name !== "Enkindle") {
-    await client.connect();
-    console.log("here");
-  }
-  req.dbClient = client;
-  req.db = client.db("Enkindle");
-  return next();
-});
-
-export default router;
+export default db;
