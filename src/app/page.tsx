e9908a6b7af5778549e19d264c45f9614da3f305
bhupen@gridflowAI <info@gridflowai.com>
2024-04-27 13:19:07 -0700

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,33 +6,45 @@ import SuggestedAuthors from "@/components/SuggestedAuthors";
 import AddQuoteDialog from "@/components/AddQuoteDialog";
 import React, { useState } from "react";
 
-export default function Home() {
+const Header = () => {
   const [open, setOpen] = useState(false);
+
+  return (
+    <div className="flex flex-row items-center justify-between px-6 py-2 border-b-2 border-opacity-25 border-battleship-gray justify">
+      <h1 className={"text-3xl font-extrabold flex-nowrap mx-6"}>
+        Enkindle
+      </h1>
+      <div className="h-[30%] w-[40%] border-2 border-black-40 rounded-lg mx-6">
+        <input className="w-[90%] h-full mx-3 my-[10px] outline-none" placeholder="Search Quotes or Authors" />
+      </div>
+      <div className="inline-flex mx-6 space-x-6 xl:mx-12">
+        <button
+          className="inline-flex items-center px-2 py-[0.5px] space-x-2 border-2 cursor-pointer rounded-xl border-opacity-20 sm:border-transparent md:border-battleship-gray md:border-opacity-20"
+          onClick={() => setOpen(true)}
+        >
+          <BsPencilSquare className="w-4 h-4 fill-eerie-black sm:w-6 sm:h-6 md:w-4 md:h-4" />
+
+          <p className="font-sans text-sm font-bold text-eerie-black sm:opacity-0 md:opacity-100">
+            Add Quote
+          </p>
+        </button>
+        <div className="relative w-12 h-12 overflow-hidden border-2 rounded-full border-battleship-gray border-opacity-30">
+          <BiUserCircle className="w-full h-full fill-battleship-gray opacity-40" />
+        </div>
+      </div>
+      <AddQuoteDialog open={open} onClose={() => setOpen(!open)} />
+
+    </div>)
+}
+
+
+
+export default function Home() {
   return (
     <main className="h-screen ">
+      <Header />
       <div className="flex flex-col h-full ">
-        <div className="flex flex-row items-center justify-between py-2 border-b-2 border-opacity-25 border-battleship-gray justify">
-          <h1 className={"mx-6 text-3xl font-extrabold flex-nowrap "}>
-            Enkindle
-          </h1>
-          <div className="inline-flex mr-12 space-x-6">
-            <button
-              className="inline-flex border-2 rounded-xl border-battleship-gray items-center  space-x-2  border-opacity-20 px-3 py-[0.5px]  cursor-pointer"
-              onClick={() => setOpen(true)}
-            >
-              <BsPencilSquare className="w-4 h-4 fill-eerie-black" />
-
-              <p className="font-sans text-sm font-bold text-eerie-black">
-                Add Quote
-              </p>
-            </button>
-            <AddQuoteDialog open={open} onClose={() => setOpen(!open)} />
-            <div className="relative w-12 h-12 overflow-hidden border-2 rounded-full border-battleship-gray border-opacity-30">
-              <BiUserCircle className="w-full h-full fill-battleship-gray opacity-40" />
-            </div>
-          </div>
-        </div>
-
+        
         <div className="inline-flex py-1 pb-4 space-x-2">
           <div className="border-r-2 border-opacity-30 border-battleship-gray w-[80%]">
             <div className="pl-[15%] px-4 ">
